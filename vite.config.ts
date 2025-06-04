e8fcc9368f7a28b1426c005ba029d82f216a55b2
piyarsquare <115601030+piyarsquare@users.noreply.github.com>
2025-06-04 12:17:41 -0400

    Fix pages deploy (#8)

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/animath/',
   plugins: [react()],
-  resolve: {
-    alias: {
-      '/animath': new URL('.', import.meta.url).pathname,
-    },
-  },
 });
