0133b16f46b4271beb6abd18087d464862a35103
piyarsquare <115601030+piyarsquare@users.noreply.github.com>
2025-06-04 12:01:04 -0400

    Fix index.html script path for GitHub pages (#6)

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/animath/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      '/animath': new URL('.', import.meta.url).pathname,
+    },
+  },
 });
