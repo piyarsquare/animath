b5fb6a3d485b2be3df4e81a744d1c0d031dd071d
piyarsquare <115601030+piyarsquare@users.noreply.github.com>
2025-06-05 16:21:23 -0400

    fix: inline quaternion glsl (#51)

--- src/animations/ComplexParticles/shaders/index.ts
+++ src/animations/ComplexParticles/shaders/index.ts
@@ -1,6 +1,19 @@
 export const vertexShader = `
 // DOMAIN–COLORING VERTEX SHADER
-#include "quat.glsl"
+struct quat { float w; vec3 v; };
+
+quat quatMul(in quat a, in quat b){
+    return quat(
+        a.w*b.w - dot(a.v, b.v),
+        a.w*b.v + b.w*a.v + cross(a.v, b.v)
+    );
+}
+
+vec4 quatRotate4D(in vec4 p, in quat a, in quat b){
+    quat q = quat(p.w, p.xyz);
+    quat r = quatMul(quatMul(a, q), quat(b.w, -b.v));
+    return vec4(r.v.x, r.v.y, r.v.z, r.w);
+}
 uniform float time;
 uniform int   functionType;
 uniform float globalSize;
