04d4566baa08f4c383771d8861e12a161661870e
piyarsquare <115601030+piyarsquare@users.noreply.github.com>
2025-06-11 18:40:05 -0400

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const routes: Record<string, JSX.Element> = {
   '/': <App />,
   '/fractals': <FractalsGPU />,
   '/correspondence': <Correspondence />,
-  '/roots': <ComplexRoots />
+  '/roots': <ComplexRoots />,
   '/multibranch': <ComplexMultibranch />
 };
 
